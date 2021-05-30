8db9befd093217bb63f60cc0a41c8cb30d4ed4c3
木村智史 <kimura-satoshi@m2.sevenbank.co.jp>
2021-05-30 13:34:54 +0900

    [add]javaファイル追加

---
+++ JavaSilver.java
@@ -0,0 +1,43 @@
+//10-41　×
+
+public class Main {
+    public static void main (String[] args){
+        //ここに何が入るか
+    }
+} 
+
+class Sample {
+    void test() {
+        System.out.println("AAAA");
+    }
+}
+
+new Sample.test ( );
+
+
+
+//11-43
+
+switch (key) {
+    case value:
+    System.out.println("AAAA");
+        break;
+
+    default:
+    System.out.println("BBBB");
+        break;
+}
+
+//11-18
+
+
+//6-3
+
+
+//
+
+
+//
+
+
+
